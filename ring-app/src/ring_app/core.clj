e805ff5189ac254bfb3b8a86d4a87a8316082a4b
anthonygraca <anthonygraca@gmail.com>
2022-08-08 08:50:01 -0700

    added wrapper middleware function that takes in a handler and returns another handler

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,5 +1,7 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn foo
   "I don't do a whole lot."
@@ -7,14 +9,23 @@
   (println x "Hello, World!"))
 
 (defn handler [request-map]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> your IP is: "
-              (:remote-addr request-map)
-              "</body></html>")})
+  (response/response
+   (str "<html><body>Hello, your IP is: "
+        (:remote-addr request-map)
+        "</body></html>"))
+  )
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
 
 (defn -main []
   (jetty/run-jetty
-   handler
+   (-> handler
+       var
+       wrap-nocache
+       wrap-reload)
    {:port 3000
     :join? false}))
