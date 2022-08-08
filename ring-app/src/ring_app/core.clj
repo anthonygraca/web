0803dd6a3bb109b0f7e089d0cc8331e6ce1067f7
anthonygraca <anthonygraca@gmail.com>
2022-08-07 18:40:14 -0700

    added Ring's jetty adapter so we can start a web server. jetty takes in our handler function and a map to open an HTTP port

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,4 +1,5 @@
-(ns ring-app.core)
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]))
 
 (defn foo
   "I don't do a whole lot."
@@ -10,5 +11,10 @@
    :headers {"Content-Type" "text/html"}
    :body (str "<html><body> your IP is: "
               (:remote-addr request-map)
-              "</body></html>")}
-  )
+              "</body></html>")})
+
+(defn -main []
+  (jetty/run-jetty
+   handler
+   {:port 3000
+    :join? false}))
